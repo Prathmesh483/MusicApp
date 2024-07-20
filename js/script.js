ee05b31bdeca46151fbaa8ae7a70cd0c8762cedc
Prathmesh483 <137476274+Prathmesh483@users.noreply.github.com>
2024-07-20 16:03:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
+    let a = await fetch(`https://669b8c62b2a59445929e9195--carvanmusic.netlify.app/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
